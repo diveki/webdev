82968ee54a5185f5312ab56376572f98f6ba09aa
Divéki Zsolt PhD <divekizs@ELI-ALPS.local>
2020-04-13 14:19:31 +0200

    textinput form field widget tested but regected

--- django_projects/zizifitness/accounting/forms.py
+++ django_projects/zizifitness/accounting/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.forms import TextInput
 
 from .views import *
 import datetime
@@ -13,6 +14,9 @@ class IncomeItemsUpdateForm(forms.ModelForm):
         fields = '__all__'
         permission_required = ('accounting.can_see_own_financials')
         success_url = reverse_lazy('accounting:incomeitems_list')
+        # widgets = {
+        #     'client': TextInput()
+        # }
 
     def __init__(self, user, *args, **kwargs):
         super(IncomeItemsUpdateForm, self).__init__(*args, **kwargs)
